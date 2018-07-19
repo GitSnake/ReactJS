c5fd8b05dabb44add231f6725e97bfb07345f4eb
gitSnake <mastermage1@gmail.com>
2018-07-19 14:09:27 -0400

    Removed Square component and added Square function

--- tic-tac-toe-app/src/index.js
+++ tic-tac-toe-app/src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-      	className="square"
-      	onClick={() => this.props.onClick()}
-      	>
-        {this.props.value}
-      </button>
-    );
-  }
+
+function Square(props) {
+	return (
+		<button className="square" onClick={props.onClick}>
+			{props.value}
+		</button>
+	);
 }
 
 class Board extends React.Component {
@@ -32,7 +28,7 @@ handleClick(i) {
     	<Square
     		value={this.state.squares[i]}
     		onClick={() => this.handleClick(i)}
-    		/>;
+    		/>
     	);
   }
 
