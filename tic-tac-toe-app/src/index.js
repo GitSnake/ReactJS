23f1cfafef6f71818f61ffe908da6e9c3fe97508
gitSnake <mastermage1@gmail.com>
2018-07-18 23:48:02 -0400

    Rendered Game and Board componants

--- tic-tac-toe-app/src/index.js
+++ tic-tac-toe-app/src/index.js
@@ -43,3 +43,26 @@ render() {
 		);
 	}
 }
+
+class Game extends React.Component {
+	render() {
+		return (
+			<div className="game">
+				<div className="game-board">
+					<Board />
+				</div>
+				<div className="game-info">
+					<div>{/* status */}</div>
+					<ol>{/* TODO */}</ol>
+				</div>
+			</div>
+		);
+	}
+}
+
+// ===================================================
+
+ReactDOM.render(
+	<Game />,
+	document.getElementById('root')
+	);
