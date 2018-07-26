7458a84771b85f9d362ea8cc61f78ba52d6f1e36
gitSnake <mastermage1@gmail.com>
2018-07-26 01:21:53 -0400

    Added hr tag to Product component

--- 1-react-basics/script.js
+++ 1-react-basics/script.js
@@ -13,6 +13,7 @@ let Product = React.createClass({
   			<p>Android - $199</p>
   			<button onClick={this.buy}>Buy</button>
   			<h3>Qty: {this.state.qty} item(s)</h3>
+  			<hr />
   		</main>
   	);
   }
